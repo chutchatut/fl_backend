aff0a479a6c19f5ce4c36c79cb51c76c93552505
chutchatut <33798875+chutchatut@users.noreply.github.com>
2021-05-15 19:38:14 +0700

    Change epoch capture and remove loss parser

--- utils/parser.py
+++ utils/parser.py
@@ -2,9 +2,7 @@ import re
 
 
 EPOCH_TIME_CAPTURE = r"Total time for fitting: \d+.\d+s"
-EPOCH_CAPTURE = r"Epoch: [0-9]+\/[0-9]+, (?:\w+: +[ 0-9]+\.[0-9s ]+)+"
-TRAIN_CAPTURE = r"Epoch: [0-9]+\/[0-9]+, Iter: [0-9]+\/[0-9]+ \[=* *\]  train_accuracy: [0-9]+\.[0-9]+  train_loss: [0-9]+\.[0-9]+  time: [0-9]+\.[0-9]+s"
-
+EPOCH_CAPTURE = r"Epoch: [0-9]+\/[0-9]+, (?:\w+: +-?[0-9]+\.[0-9s ]+)+"
 
 def parse_rounds(rounds, func):
     out = []
@@ -41,19 +39,11 @@ def parse_metrics(r):
     return metrics_dct
 
 
-def parse_losses(r):
-    return {"loss": re.findall(r'[0-9]+\.[0-9]+', re.findall(TRAIN_CAPTURE, r)[-1])[1]}
-
-
 def parse_log(log):
     
     rounds = log.split('Get global model for round:')[:0:-1]
     
     times = parse_rounds(rounds, parse_time)
-    metrics = parse_rounds(rounds, parse_metrics)      
-    losses = parse_rounds(rounds, parse_losses)
-    
-    for i in range(len(metrics)):
-        metrics[i].update(losses[i]) 
+    metrics = parse_rounds(rounds, parse_metrics)
     
     return {'round_time': times, 'metrics': metrics}
